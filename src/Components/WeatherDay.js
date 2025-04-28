4c3ab7c61159eab0b0a82b90e110e6fc8f412cf2
Максим_Мамков <madumax1809@gmail.com>
2025-04-28 19:16:31 +0300

    fix images

--- src/Components/WeatherDay.js
+++ src/Components/WeatherDay.js
@@ -33,14 +33,14 @@ function WeatherDay({temp, feelslike, humidity, snow, cloudcover, wind, pressure
                 <h4 className='ml-15'>Ощущается как {Math.round((feelslike - 32) * 5 / 9)}°C</h4>
                 <div className='mt-30'>
                     <div className='d-flex flex-row'>
-                        <img width={48} height={48} src={`${process.env.PUBLIC_URL}/img/sunrise.png`} alt="Восход" />
+                        <img width={48} height={48} src={`/img/sunrise.png`} alt="Восход" />
                         <div className='d-flex flex-column justify-center ml-15'>
                             <b>Восход</b>
                             <p>{sunrise.split(':').slice(0, 2).join(':')}</p>
                         </div>
                     </div>
                     <div className='d-flex flex-row mt-15'>
-                        <img width={48} height={48} src={`${process.env.PUBLIC_URL}/img/sunset.png`} alt="Закат" />
+                        <img width={48} height={48} src={`/img/sunset.png`} alt="Закат" />
                         <div className='d-flex flex-column justify-center ml-15'>
                             <b>Закат</b>
                             <p>{sunset.split(':').slice(0, 2).join(':')}</p>
@@ -57,24 +57,24 @@ function WeatherDay({temp, feelslike, humidity, snow, cloudcover, wind, pressure
             <div className={styles.table}>
                 <div className={styles.row}>
                     <div className={styles.cell}>
-                        <img src={`${process.env.PUBLIC_URL}img/humidity.png`} alt="Влажность" />
+                        <img src={`img/humidity.png`} alt="Влажность" />
                         <p className={styles.value}>{humidity}%</p>
                         <b>Влажность</b>
                     </div>
                     <div className={styles.cell}>
-                        <img height={50} src={`${process.env.PUBLIC_URL}img/wind.png`} alt="Ветер" />
+                        <img height={50} src={`img/wind.png`} alt="Ветер" />
                         <p>{wind} м/с</p>
                         <b>Ветер</b>
                     </div>
                 </div>
                 <div className={styles.row}>
                     <div className={styles.cell}>
-                        <img src={`${process.env.PUBLIC_URL}img/pressure.png`} alt="Давление" />
+                        <img src={`img/pressure.png`} alt="Давление" />
                         <p>{Math.round(pressure / 1.333)} мм рт.ст</p>
                         <b>Давление</b>
                     </div>
                     <div className={styles.cell}>
-                        <img src={`${process.env.PUBLIC_URL}img/uv.png`} alt="УФ индекс" />
+                        <img src={`img/uv.png`} alt="УФ индекс" />
                         <p>{uv}</p>
                         <b>УФ</b>
                     </div>
